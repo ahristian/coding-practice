f20a435941faa58d7800a8112e56c715fa3e1d29
ahristian <hristian.andrei@gmail.com>
2020-08-24 11:35:57 -0600

    solved

---
+++ startHi/startHi.js
@@ -0,0 +1,10 @@
+function startHi(str){
+  let hi, positionLetter;
+  if ( (str[0] === 'h') && (str[1] === 'i')){
+    return true;
+  }
+  else {
+    return false;}
+}
+
+console.log(startHi('hi there'));
