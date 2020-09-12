a6694911642e42ff8ae1d5e5722b3d52f513dec8
ahristian <hristian.andrei@gmail.com>
2020-09-11 20:57:49 -0600

    solved

---
+++ nearHundred/nearHundred.js
@@ -0,0 +1,10 @@
+function nearHundred(n){
+  if (n >= 190 && n <= 210) {
+    return true;}
+  if (n >= 90 && n <= 110){
+    return true;}
+  else { return false;}
+
+}
+
+console.log(nearHundred(93))
